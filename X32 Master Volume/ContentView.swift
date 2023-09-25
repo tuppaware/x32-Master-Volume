eba2fd3c973e1a113d92eac10ac3e58ca52f5be7
Adam Ware <tuppaware@gmail.com>
2023-09-25 11:21:20 +1000

    Initial Commit

---
+++ X32 Master Volume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  X32 Master Volume
+//
+//  Created by Adam Ware on 25/9/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ X32 Master Volume/X32_Master_VolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  X32_Master_VolumeApp.swift
+//  X32 Master Volume
+//
+//  Created by Adam Ware on 25/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct X32_Master_VolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
